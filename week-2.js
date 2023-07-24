e474a4493fa6e57d33cba7f3df046529025775b3
nickonyi <nickoluoch1130@gmail.com>
2023-07-24 06:40:06 +0300

    code wars

--- week-2.js
+++ week-2.js
@@ -4,7 +4,3 @@ function removeEveryOther(arr) {
     return arr.filter((_, index) => index === 0 || index % 2 === 0);
 
 }
-
-function removeEverySecondElement(arr) {
-    return arr.filter((_, index) => index === 0 || index % 2 === 0);
-}
