ff6a6b2403deec9c763dd08f33f09de38973e49b
nickonyi <nickoluoch1130@gmail.com>
2023-07-25 10:16:53 +0300

    code wars

--- week-2.js
+++ week-2.js
@@ -4,3 +4,35 @@ function removeEveryOther(arr) {
     return arr.filter((_, index) => index === 0 || index % 2 === 0);
 
 }
+
+
+
+function greet(language) {
+    //create the database
+    const database = {
+            english: 'Welcome',
+            czech: 'Vitejte',
+            danish: 'Velkomst',
+            dutch: 'Welkom',
+            estonian: 'Tere tulemast',
+            finnish: 'Tervetuloa',
+            flemish: 'Welgekomen',
+            french: 'Bienvenue',
+            german: 'Willkommen',
+            irish: 'Failte',
+            italian: 'Benvenuto',
+            latvian: 'Gaidits',
+            lithuanian: 'Laukiamas',
+            polish: 'Witamy',
+            spanish: 'Bienvenido',
+            swedish: 'Valkommen',
+            welsh: 'Croeso'
+        }
+        //loop over the database and compare values
+    for (const lang in database) {
+        if (lang === language) {
+            return database[lang];
+        }
+    }
+    return "Your function should have returned 'Welcome'. Try again.";
+}
