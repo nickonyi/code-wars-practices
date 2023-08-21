108419d26f1a0c663f9f5963aaf455e000386b17
nickonyi <nickoluoch1130@gmail.com>
2023-08-21 18:37:35 +0300

    week 6

---
+++ week6.js
@@ -0,0 +1,11 @@
+function findMultiples(integer, limit) {
+    //your code here
+    const multiples = [];
+
+    for (let i = integer; i <= limit; i++) {
+        if (i % integer == 0) {
+            multiples.push(i);
+        }
+    }
+    return multiples;
+}
