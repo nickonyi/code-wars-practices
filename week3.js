78cd7d2a63ba6cf980d0361c4a75f31aa54cffa4
nickonyi <nickoluoch1130@gmail.com>
2024-05-13 16:37:56 +0300

    code

--- week3.js
+++ week3.js
@@ -37,3 +37,12 @@ function sayHello(name, city, state) {
 
     return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
 }
+
+function remainder(n, m) {
+    // Divide the larger argument by the smaller argument and return the remainder
+    const max = Math.max(n, m);
+    const min = Math.min(n, m);
+
+    return max % min;
+
+}
